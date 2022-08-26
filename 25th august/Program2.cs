b96d95529a69a03e2726a4035f91c9b921770e71
reddibhavyasri <reddibhavyasri@gmail.com>
2022-08-26 10:44:41 +0530

    bhavya

---
+++ 25th august/Program2.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace aug25asnmnt2
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+           string path = @"E:\aug25";
+            Console.WriteLine("Enter the file name");
+            string fname = Console.ReadLine();
+
+            File.Create(path+fname);
+            fname = string.Concat(path, fname);
+
+           //  File.Create(fname);//
+            // Console.WriteLine("file created successfully...");
+
+           // string s = "this is bhavya";//
+           // File.WriteAllText(fname, s);
+             //string k = "this is the appended text";
+             //File.AppendAllText(fname, k);
+           // Console.WriteLine("file created successfully with contents...");
+             //File.Copy(fname, "copiedFile.txt");
+            //File.Move(fname, "MovedFile.txt");
+          /* FileInfo fileInfo = new FileInfo(fname);
+            Console.WriteLine(fileInfo.FullName + "  Full Name");
+             Console.WriteLine(fileInfo.DirectoryName + " DirectoryName");
+            Console.WriteLine(fileInfo.CreationTime);
+            Console.WriteLine(fileInfo.Extension);*/
+            File.Delete(fname);
+            Console.WriteLine("file deleted successfully");
+
+            Console.ReadLine();
+        }
+    }
+}
+        
+    
+
---
+++ 25th august/Program4.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aug25asnmnt4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("C drive info");
+            DriveInfo info = new DriveInfo("C");
+            Console.WriteLine(info.Name);
+            Console.WriteLine(info.TotalSize);
+            Console.WriteLine(info.DriveType);
+            Console.WriteLine(info.DriveFormat);
+            Console.WriteLine("----------------------------");
+            DriveInfo[] dinfo = DriveInfo.GetDrives();
+            foreach (var item in dinfo)
+            {
+                Console.WriteLine(item.Name);
+            }
+            Console.ReadLine();
+        }
+    }
+}
