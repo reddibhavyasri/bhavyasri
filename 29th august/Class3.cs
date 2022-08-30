53f8d2309a855e9229eb44b5d7605fef14f77f23
reddibhavyasri <reddibhavyasri@gmail.com>
2022-08-30 09:15:54 +0530

    bhavya

---
+++ 29th august/Class3.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aug29asnmnt3
+{
+    internal class Queue
+    {
+
+        internal static readonly int max = 1000;
+        internal int[] queue = new int[max];
+        int front, rear, capacity;
+        public Queue(int c)
+        {
+            front = rear = 0;
+            capacity = c;
+
+        }
+
+        internal void Enqueue(int data)
+        {
+            if (capacity == rear)
+            {
+                Console.WriteLine("\n queue is full");
+                return;
+            }
+            else
+            {
+                queue[++rear] = data;
+
+            }
+        }
+        internal void Dequeue()
+        {
+
+            if (front == rear)
+            {
+                Console.Write("\n Queue is empty");
+                return;
+            }
+            else
+            {
+                for (int i = 0; i < rear - 1; i++)
+                {
+                    queue[i] = queue[i + 1];
+                }
+
+
+                if (rear < capacity)
+                    queue[rear] = 0;
+
+
+                rear--;
+            }
+            return;
+        }
+
+        internal void Display()
+        {
+            int i;
+            if (front == rear)
+            {
+                Console.Write("\n Queue is Empty");
+                return;
+            }
+            for (i = front; i < rear; i++)
+            {
+                Console.Write(" {0} ", queue[i]);
+            }
+            return;
+        }
+
+
+    }
+}   
+
+
+
+
---
+++ 29th august/Program3.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aug29asnmnt3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Queue q = new Queue(4);
+
+
+            q.Display();
+
+            q.Enqueue(20);
+            q.Enqueue(30);
+            q.Enqueue(40);
+            q.Enqueue(50);
+
+
+            q.Display();
+
+            q.Enqueue(60);
+
+            q.Display();
+
+            q.Dequeue();
+            q.Dequeue();
+            Console.Write(" \n after two node deletion");
+
+            q.Display();
+
+            Console.ReadLine();
+        }
+
+
+    }
+}
+    
+
