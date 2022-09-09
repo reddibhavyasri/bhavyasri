61a0150ae0ce957f3fe5f61435f539baf173ba68
reddibhavyasri <reddibhavyasri@gmail.com>
2022-09-09 14:49:01 +0530

    bhavya

---
+++ 8th september/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classlibrary
+{
+    public class dept
+    {
+        private int deptno;
+        public int Deptno
+        {
+            get { return deptno; }
+            set { deptno = value; }
+        }
+        private string deptname;
+        public string  Deptname
+        {
+            get { return deptname; }
+            set { deptname = value; }
+        }
+        private string deptlocation;
+        public string Deptlocation
+        {
+            get { return deptlocation; }
+            set
+            {
+                deptlocation = value;
+            }
+        }
+    }
+}
---
+++ 8th september/Employee.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classlibrary
+{
+    public class Employee
+    {
+        private int empid;
+        public int Empid
+        {
+            get { return empid; }
+            set
+            {
+                empid = value;
+            }
+        }
+        private string empname;
+        public string Empname
+        {
+            get { return empname; }
+            set
+            {
+                empname = value;
+            }
+        }
+        private int deptno;
+        public int Deptno
+        {
+            get { return deptno; }
+            set
+            {
+                deptno = value;
+            }   
+        }
+        private int sal;
+        public int Sal
+        {
+            get { return sal; }
+            set
+            {
+                sal = value;
+            }
+        }
+
+        private string mgr;
+        public string Mgr
+        {
+            get { return mgr; }
+            set
+            {
+                mgr = value;
+            }
+        }
+    }
+}
