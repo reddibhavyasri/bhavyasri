ce3057a37a58007df9a42d096ff81abf4051aaad
reddibhavyasri <reddibhavyasri@gmail.com>
2022-09-12 22:28:23 +0530

    bhavya

---
+++ 12th september/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+    public class Caluculator
+    {
+        private double number;
+        public double Number
+        {
+            get { return number; }
+            set { number = value; }
+        }
+        public void Cube(double number)
+        {
+            Number = number;
+            double ans = number * number * number;
+            Console.WriteLine("The Cube of number is " + ans);
+        }
+        public void sqrt(double number)
+        {
+            Number = number;
+            Double ans=Math.Sqrt(number);
+            Console.WriteLine("The Square root of number is " + ans);
+
+
+        }
+           
+        }
+    }
+
---
+++ 12th september/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sep12thasnmnt1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "C:\\Users\\User\\source\\repos\\calculator\\bin\\Debug\\calculator.dll";
+            Assembly asm = Assembly.LoadFrom(path);
+            Type[] types = asm.GetTypes();
+            Type t = null;
+            foreach (var item in types)
+            {
+                t = asm.GetType(item.FullName);
+                object obj = Activator.CreateInstance(t);
+                item.InvokeMember("Cube", BindingFlags.Public | BindingFlags.Instance | BindingFlags.InvokeMethod, null, obj, new object[] { 12 });
+                item.InvokeMember("sqrt",BindingFlags.Public|BindingFlags.Instance | BindingFlags.InvokeMethod, null, obj, new object[] { 14 });    
+                break;
+
+
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
