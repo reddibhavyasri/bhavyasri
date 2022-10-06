bb89e04a1f4b9c77bf555c50b491360ac2e9fefa
reddibhavyasri <reddibhavyasri@gmail.com>
2022-10-06 22:33:42 +0530

    bhavya

---
+++ WebApplication2/Controllers/ProductsController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult DeleteProduct()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Deleteproduct(int id)
+        {
+            return View();
+        }
+        public ActionResult SearchProduct(int id)
+        {
+            return Content("Tried to find the product id " + id);
+        }
+        public ActionResult AddProduct()
+        {
+            return RedirectToAction("Index");
+        }
+        public ActionResult GoToGoogle()
+        {
+            return Redirect("https://www.google.com");
+        }
+        public ActionResult GoToIndexPageofHomeController()
+        {
+            return RedirectToAction("Index","Home");
+        }
+    }
+}
