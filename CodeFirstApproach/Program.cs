ba488080e3ccee239a5b51e8e21e284b9a698fe9
reddibhavyasri <reddibhavyasri@gmail.com>
2022-10-21 10:17:40 +0530

    commit

---
+++ CodeFirstApproach/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeFirstDALDemo;
+
+namespace CodeFirstApproach
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            MyContext context = new MyContext();
+            context.studentsTable.Add(new Student{ Rollno=1,Studname="ananth"});
+            context.SaveChanges();
+            Console.WriteLine("Done");
+            Console.ReadLine(); 
+        }
+    }
+}
